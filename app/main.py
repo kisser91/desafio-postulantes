db8a492e0f78837bdc369d544a36a6314f49abe9
Nahuel Kisser <81387542+kisser91@users.noreply.github.com>
2022-04-21 23:43:11 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+
+@app.get("/ruts")
+async def get_ruts():
+    return {"ruts:": "loren ipsum"}
